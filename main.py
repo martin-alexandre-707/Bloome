6a14be9dc4936f4f36b59db16561de7bb0329db2
martin-alexandre-707 <martinalexandre2033@gmail.com>
2026-05-22 14:22:08 -0300

    Criação do esqueleto do menu e regras do negocio

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 from config.db import criar_conexao
 from repositories.insumos import insert_insumos
+from repositories.auth import login 
+import os
 
-nome = input("Digite o nome do insumo: ").strip
-valor_unitario = input("Digite o valor unitario do insumo: ")
-quantidade_estoque = input("Digite a quantidade do estoque:  ")
-categoria = input("Digite a categoria: ")
-insert_insumos(nome, valor_unitario, quantidade_estoque, categoria)
+usuario=input("Digite o usuario: ")
+password= input("Digite a sua senha: ")
+user_logged = login(usuario, password)
+
+# Aba insumos
+#   Cadastrar novo insumo, colocando o nome (UNICO e com limite de 255 caractareses que nem o banco de dados), o valor unitario (pode ser >= 0 mas nn pode ser negativo) (metro ou unidade), quantidade a ser adicionada, e categoria que é o insumo ex: cordão, fecho e pingente.
+#
+# Ver estoque (select * from insumos)
+#       
+#
+# Editar estoque
+#   adicionar ou remover quantidade de insumo já existente
+#
+#
+#
+#
+
+
+
+if user_logged:
+    print("Seja bem-vindo!")
+    while True:
+        print("----MENU----/n1-Cadastrar insumos  /n2-")    
+        opcao= input("Digite a opção que deseja acessar")
+        match opcao:
+            case "1":
+                pass
+            case "2":
+                pass
+            case "3":
+                pass
+            case "4":
+                pass
+else:
+    print("Usuário ou senha inválidos")
--- repositories/auth.py
+++ repositories/auth.py
@@ -1,10 +1,10 @@
-from config import criar_conexao
+from config.db import criar_conexao
 
 def login (usuario: str, senha : str ):
     try:
         con= criar_conexao()
         cursor= con.cursor()
-        cursor.execute("SELECT * FROM usuarios where email=%s and password=%s", (usuario, senha))
+        cursor.execute("SELECT * FROM usuarios where usuario=%s and senha=%s", (usuario, senha))
         return cursor.fetchone()
     except Exception as e:
         print(e)
